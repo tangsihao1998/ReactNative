49fee8a71753311c806eb0a447ccd8f9748e2e26
Sĩ Hào <tangsihao1998@gmail.com>
2019-08-17 23:48:03 +0700

    add list view for favorite list

--- Client/src/components/favorite/favorite.js
+++ Client/src/components/favorite/favorite.js
@@ -7,7 +7,7 @@
  */
 
 import React, { Fragment } from 'react';
-import { View, StyleSheet, Text, ScrollView, Alert, Image, TouchableWithoutFeedback, TouchableHighlight } from 'react-native';
+import { View, StyleSheet, Text, ScrollView, Alert, Image, TouchableWithoutFeedback, TouchableHighlight, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 
 //import component
@@ -19,11 +19,39 @@ class Favorite extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  _keyExtractor = (item, index) => item.id;
+
+  _renderItem = ({item, index}) => {
+    const image = 'https://image.tmdb.org/t/p/w500' + item.poster_path;
+    return (
+        <View style={styles.slide}>
+          <Image style={styles.poster} source={{uri: image}}/>
+          <View style={styles.movieContent}>
+            <Text style={styles.title}>{item.title}</Text>
+            {item.adult ? <View style={styles.c18}>
+                            <Text style={styles.age18}>C18</Text>
+                          </View> : <View style={styles.c13}>
+                                      <Text style={styles.age13}>C13</Text>
+                                    </View>}
+            <Text style={styles.releaseDate}>{ item.release_date }</Text>
+          </View>
+        </View>
+      );
+  }
 //   getFavoriteList
   render() {
+    const {favoriteList} = this.props;
     return (
       <Fragment>
         <View style={styles.container}>
+          <ScrollView >
+            <FlatList
+              data={favoriteList}
+              keyExtractor={this._keyExtractor}
+              renderItem={this._renderItem}
+            />
+          </ScrollView>
         </View>
       </Fragment>
     );
@@ -33,6 +61,56 @@ class Favorite extends React.Component {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#ffffff",
+  },
+  slide: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: 'space-around',
+    fontFamily: 'Cochin'
+  },
+  poster: {
+    width: 100,
+    height: 100,
+    marginBottom: 10,
+  },
+  movieContent: {
+    
+  },
+  title: {
+    color: '#000000',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    fontSize: 14,
+  },
+  c13: {
+    width: 22,
+    borderColor: '#c0be2a',
+    borderWidth: 1,
+    borderRadius: 4,
+    fontSize: 10,
+  },
+  age13: {
+    color: '#c0be2a',
+    textTransform: 'uppercase',
+    fontSize: 10,
+  },
+  c18: {
+    width: 22,
+    borderColor: '#c0412a',
+    borderWidth: 1,
+    borderRadius: 4,
+    fontSize: 10,
+  },
+  age18: {
+    color: '#c0412a',
+    textTransform: 'uppercase',
+    fontSize: 10,
+  },
+  releaseDate: {
+    color: '#000000',
+    fontSize: 12,
   },
 });
 
